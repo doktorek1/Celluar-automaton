e4c59eecb472959bafc697cb8a06c19e893bb9dd
doktorek1 <757896@gmail.com>
2014-03-12 17:06:23 +0100

    Nic takiego 2

--- grid.c
+++ grid.c
@@ -4,8 +4,8 @@
 void alloc_grid(int x, int y)
 {
 	int i;
-	wskaznik->tab1 = malloc( x*y*sizeof (int));
-	wskaznik->tab2 = malloc( x*y*sizeof (int));
+	wskaznik->tab1 = malloc( x * y * sizeof (int));
+	wskaznik->tab2 = malloc( x * y * sizeof (int));
 	wskaznik->n = x * y;
 	for(i = 0; i<wskaznik->n; i++)
 		{
@@ -13,18 +13,14 @@ void alloc_grid(int x, int y)
                 wskaznik->tab2[i] = 0;
 		}
 }
-void read_from_file(FILE * in)
+void read_from_file_and_write_to_memory(FILE * in)
 {
-	int i=0, x, y;
-	
-	fscanf(in, "%d", &x); 
-	fscanf(in, "%d", &y); 
+	int i=0, x, y, k;
+	fscanf(in, "%d %d", &x, &y);  
+	k = y;
 	alloc_grid(x, y);
 	while(fscnaf(in, "%d %d", &x, &y) == 2)
 	     {
-	     wskaznik->tab1[12] = x;
- 	     *(wskaznik->tab2) = y;
-	     wskaznik->tab1++;
-	     wskaznik->tab2++;	     
+	     wskaznik->tab1[(x-1)*k + y - 1] = 1;	     
              }
 }
--- grid.h
+++ grid.h
@@ -2,14 +2,14 @@
 #define GRID_H
 
 #include <stdio.h>
-/*kjvbvb*/
+
 typedef struct {
 	int *tab1;
 	int *tab2;
 	int n;
 } * wskaznik;
 
-void read_from_file(FILE * in);
+void read_from_file_and_write_to_memory(FILE * in);
 
 #endif
 
